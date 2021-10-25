4567a1a5156f28edc6f9f3fffe16c5f8e4889a24
Michael Newman <mjnewman@pomp.com>
2021-10-25 14:06:31 -0400

    Updated container_setup.sh scripts

--- scripts/container_setup.sh
+++ scripts/container_setup.sh
@@ -9,3 +9,4 @@ echo "alias ll='ls -la'" >> /root/.bashrc
 echo "alias www='cd /var/www'" >> /root/.bashrc
 echo "alias html='cd /var/www/html'" >> /root/.bashrc
 echo "alias log_php_errors='echo \"PHP-FPM ERROR LOG >>\" > /var/www/html/var/log/php-error.log; clear; tail -f /var/www/html/var/log/php-error.log'" >> /root/.bashrc
+echo "alias log_pimcore_errors='echo \"PIMCORE ERROR LOG >>\" > /var/www/html/var/log/dev.log; clear; tail -f /var/www/html/var/log/dev.log'" >> /root/.bashrc
